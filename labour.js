e6d9f6ac7827ba9b8cd0eb2f9f4e43ef6c4e5f9c
seanb0505 <seanmbarbour@gmail.com>
2026-05-11 17:37:41 -0400

    Update labour.js

--- labour.js
+++ labour.js
@@ -1,1 +1,18 @@
+function calculateLabour(inputs) {
 
+  const totalHeads =
+    inputs.newHeads + inputs.relocateHeads;
+
+  const labourHours =
+    totalHeads * inputs.hoursPerHead;
+
+  const labourCost =
+    labourHours * inputs.labourRate;
+
+  return {
+    totalHeads,
+    labourHours,
+    labourCost
+  };
+
+}
